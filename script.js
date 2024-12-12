eb1250e770b239b5b9af6fe35d5d68b6ee681d00
hencci <hencci20@gmail.com>
2024-12-12 13:26:56 +0100

    Select DOM and save in variables

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const numKeys = document.querySelectorAll('.numKey');
+const opKeys = document.querySelectorAll('.opKey');
+const clearKeys = document.querySelectorAll('.clearKey');
+const equalTo = document.querySelector('#equal');
+
+const display = document.querySelector('.display');
+const lowerDisplay = document.querySelector('.lowerDisplay');
+const upperDisplay = document.querySelector('.upperDisplay');
+
 let firstNumber = null;
 let secondNumber = null;
 let operator = null;
